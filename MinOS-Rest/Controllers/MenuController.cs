f9f97951ef072bab1324a666a0f8536312f40629
Wesley Santos <wesleysotnas64@gmail.com>
2023-10-21 19:30:42 -0300

    first commit

---
+++ MinOS-Rest/Controllers/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MinOS_Rest_API.Models;
+
+namespace MinOS_Rest_API.Controllers
+{
+    [Route("menu/")]
+    [ApiController]
+    public class MenuController : ControllerBase
+    {
+        public MenuController() { }
+
+        [HttpGet("get-all-items")]
+        public IActionResult GetAllItems()
+        {
+            List<Item> menu = new List<Item>();
+
+            Item i1 = new Item();
+            i1.Id = 1;
+            i1.Name = "Baião de Dois";
+            i1.Price = 6.5f;
+
+            Item i2 = new Item();
+            i2.Id = 2;
+            i2.Name = "Carne de Sol";
+            i2.Price = 10.25f;
+
+            menu.Add(i1);
+            menu.Add(i2);
+
+            return Ok(menu);
+        }
+    }
+}
---
+++ MinOS-Rest/Models/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace MinOS_Rest_API.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public float Price {  get; set; }
+}
+
